bdb0d8545fa7f6a374ed68f9945ba8e1744fadc1
Score_Under <seejay.11@gmail.com>
2015-06-06 21:15:05 +0100

    Make the install script accept a directory name

--- install.sh
+++ install.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 # Crappy install script
 
-path_has() {
-    case ":$PATH:" in
-        *:"$1":*) return 0;;
-        *) return 1;;
-    esac
-}
+install_to=$1
+if [ -z "$install_to" ]; then
+    path_has() {
+        case ":$PATH:" in
+            *:"$1":*) return 0;;
+            *) return 1;;
+        esac
+    }
 
-install_to=""
-try_path() {
-    path_has "$1" && [ -w "$1" ] &&
-        install_to="$1"
-}
+    try_path() {
+        path_has "$1" && [ -w "$1" ] &&
+            install_to="$1"
+    }
 
-try_path /usr/local/bin || try_path "$HOME/bin" || try_path "$HOME/.bin"
+    try_path /usr/local/bin || try_path "$HOME/bin" || try_path "$HOME/.bin"
+fi
 
 if [ -n "$install_to" -a -d "$install_to" ]; then
     install -v -t "$install_to" pomfclip pomfload uguuload
